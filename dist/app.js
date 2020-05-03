7966db18bc34fd1e78964c9273df3d5b62fd4634
Umberto Del Piano <umberdelpi1@gmail.com>
2020-05-03 18:39:09 +0200

    Ritocchino con Javascript su navBar

--- dist/app.js
+++ dist/app.js
@@ -93,7 +93,30 @@
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-console.log("Hello World");
+window.onload = function () {
+  jumboTron = document.getElementById("container-jumbotron");
+  jumboPosition = jumboTron.offsetHeight;
+  console.log(jumboPosition);
+};
+
+window.onscroll = function () {
+  var navBar = document.getElementById("navbar");
+  var container = document.getElementById("container");
+  var buttonBlue = document.getElementById("bottonegmail");
+
+  if (window.pageYOffset > jumboPosition / 2) {
+    container.classList.add("bordo");
+    navBar.classList.remove("bordo");
+    buttonBlue.classList.add("buttonblue");
+  } else {
+    container.classList.remove("bordo");
+    navBar.classList.add("bordo");
+    buttonBlue.classList.remove("buttonblue");
+  }
+}; // Ricaviamo coordinate jumboTron
+// Ottenuta la coordinata applichiamo la misura standard
+// Applichiamo il remove alla classe .bordo
+// Aggiungiamo la classe .bordo su .container
 
 /***/ }),
 
